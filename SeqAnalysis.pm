ded4cac23f21d706bf2d9bb7383506e7b5ffa4e4
Steven Ahrendt <sahrendt0@gmail.com>
2013-11-08 17:06:43 -0800

    New Methods

--- SeqAnalysis.pm
+++ SeqAnalysis.pm
@@ -13,12 +13,161 @@ package SeqAnalysis;
 #  [x] reverse complement	: getRevComp(str dna)
 #  [x] transcribe to RNA	: transcribe(str dna)
 #  [x] motif finding		: getMotifPos(str seq, str match)
+#  [x] 6 frame translation	: getSixFrame(str dna)
 ########################
 use strict;
 use warnings;
+use Bio::Perl;
 use base 'Exporter';  # to export our subroutines
 
-our @EXPORT = qw(getMotifPos getGC getProtMass getHammDist getRevComp transcribe); # export always
+our @EXPORT = qw(getSixFrame seqTranslate getMotifPos getGC getProtMass getHammDist getRevComp transcribe); # export always
+
+our %CODONS_3 = ("MET" => {"ATG"},
+                 "ILE" => {"ATA","ATC","ATT"},
+                 "ARG" => {"CGG","CGT","CGA","CGC","AGG","AGA"},
+                 "GLN" => {"CAG","CAA"},
+                 "HIS" => {"CAC","CAT"},
+                 "PRO" => {"CCA","CCG","CCC","CCT"},
+                 "LEU" => {"CTT","CTA","CTC","CTG","TTA","TTG"},
+                 "TRP" => {"TGG"},
+                 "CYS" => {"TGC","TGT"},
+                 "TYR" => {"TAT","TAC"},
+                 "PHE" => {"TTT","TTC"},
+                 "GLY" => {"GGG","GGT","GGC","GGA"},
+                 "GLU" => {"GAA","GAG"},
+                 "ASP" => {"GAT","GAC"},
+                 "ALA" => {"GCC","GCA","GCT","GCG"},
+                 "VAL" => {"GTA","GTC","GTG","GTT"},
+                 "SER" => {"TCA","TCT","TCG","TCC","ATC","AGT"},
+                 "LYS" => {"AAA","AAG"},
+                 "ASN" => {"AAT","AAC"},
+                 "THR" => {"ACA","ACT","ACC","ACG"},
+                 "***" => {"TGA","TAA","TAG"});
+our %CODONS_1 = ("M" => {"ATG"},
+                 "I" => {"ATA","ATC","ATT"},
+                 "R" => {"CGG","CGT","CGA","CGC","AGG","AGA"},
+                 "Q" => {"CAG","CAA"},
+                 "H" => {"CAC","CAT"},
+                 "P" => {"CCA","CCG","CCC","CCT"},
+                 "L" => {"CTT","CTA","CTC","CTG","TTA","TTG"},
+                 "W" => {"TGG"},
+                 "C" => {"TGC","TGT"},
+                 "Y" => {"TAT","TAC"},
+                 "F" => {"TTT","TTC"},
+                 "G" => {"GGG","GGT","GGC","GGA"},
+                 "E" => {"GAA","GAG"},
+                 "D" => {"GAT","GAC"},
+                 "A" => {"GCC","GCA","GCT","GCG"},
+                 "V" => {"GTA","GTC","GTG","GTT"},
+                 "S" => {"TCA","TCT","TCG","TCC","ATC","AGT"},
+                 "K" => {"AAA","AAG"},
+                 "N" => {"AAT","AAC"},
+                 "T" => {"ACA","ACT","ACC","ACG"},
+                 "***" => {"TGA","TAA","TAG"});
+
+#our %AA = ("ATG" => "M",
+#           "ATA" => "I",
+#           "ATC" => "I",
+#           "ATT" => "I",
+#           "CGG" => "R",
+#           "CGT" => "R",
+#           "CGA" => "R",
+#           "CGC" => "R",
+#           "AGG" => "R",
+#           "AGA" => "R",
+# 49                  "Q" => {"CAG","CAA"},
+# 50                  "H" => {"CAC","CAT"},
+# 51                  "P" => {"CCA","CCG","CCC","CCT"},
+# 52                  "L" => {"CTT","CTA","CTC","CTG","TTA","TTG"},
+# 53                  "W" => {"TGG"},
+# 54                  "C" => {"TGC","TGT"},
+# 55                  "Y" => {"TAT","TAC"},
+# 56                  "F" => {"TTT","TTC"},
+# 57                  "G" => {"GGG","GGT","GGC","GGA"},
+# 58                  "E" => {"GAA","GAG"},
+# 59                  "D" => {"GAT","GAC"},
+# 60                  "A" => {"GCC","GCA","GCT","GCG"},
+# 61                  "V" => {"GTA","GTC","GTG","GTT"},
+# 62                  "S" => {"TCA","TCT","TCG","TCC","ATC","AGT"},
+# 63                  "K" => {"AAA","AAG"},
+# 64                  "N" => {"AAT","AAC"},
+# 65                  "T" => {"ACA","ACT","ACC","ACG"},
+# 66                  "***" => {"TGA","TAA","TAG"});
+#);
+
+#####
+## Subroutine: seqTranslate
+#    Input: DNA string
+#    Returns: use BioPerl to translate sequence
+########
+sub seqTranslate
+{
+  my $seq = shift @_;
+  my $seq_obj = Bio::Seq->new(-seq => $seq);
+  return $seq_obj->translate->seq;
+}
+
+#####
+## Subroutine: revTrans
+#    Input: protein string
+#    Returns: total num of possible RNA strings for protein (mod 1,000,000)
+########
+sub revTrans
+{
+  my $prot = shift @_;
+  my $num_RNA = 0;
+  
+
+  return ($num_RNA%1000000);
+}
+
+#####
+## Subroutine: getSixFrame
+#    Input: string of DNA
+#    Returns: array of unique potential orfs
+########
+sub getSixFrame
+{
+  my $seq = shift @_;
+  my $rc = getRevComp($seq);
+  my @frame1 = $seq =~ /(.{3})/g;
+  my @frame2 = substr($seq,1) =~ /(.{3})/g;
+  my @frame3 = substr($seq,2) =~ /(.{3})/g;
+  my @frame4 = $rc =~ /(.{3})/g;
+  my @frame5 = substr($rc,1) =~ /(.{3})/g;
+  my @frame6 = substr($rc,2) =~ /(.{3})/g;
+  my %orfs;
+  my @frames = (\@frame1,\@frame2,\@frame3,\@frame4,\@frame5,\@frame6);
+  foreach my $frame (@frames)
+  {
+    my $len = scalar(@{$frame}); 
+    for(my $i=0;$i<$len;$i++)
+    {
+      my $codon = @{$frame}[$i];
+      if($codon eq "ATG")
+      {
+        my @tmp = ($codon);
+        #print "$codon($i)<$len>:";
+        my @sub = @{$frame}[($i+1)..($len-1)];
+        #print "@sub";
+        my $j=0;
+        while($j < scalar(@sub))#   $j<scalar(@sub))
+        {
+          #print "$sub[$j]:";
+          if($sub[$j] =~ /TAA|TAG|TGA/)
+          {
+            $orfs{seqTranslate(join("",@tmp))}++;
+            last;
+          }
+          push (@tmp,$sub[$j]);
+          $j++;
+        }
+        #print "\n";
+      }
+    }
+  }
+  return %orfs;
+}
 
 #####
 ## Subroutine: getMotifPos
