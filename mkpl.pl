850c0b824778134ba51e63c84d2231916f930b36
Steven Ahrendt <sahrendt0@gmail.com>
2013-11-15 17:40:03 -0800

    Setup skeleton Perl script

---
+++ mkpl.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Time::Piece;
+
+my $date = Time::Piece->new->strftime('%m.%d.%Y');
+
+my $input = shift;
+
+open(OUT,'>',$input) or die "Can't open $input\n";
+print OUT '#!/usr/bin/perl
+# Script '.$input.'
+# Description: 
+# Author: Steven Ahrendt
+# email: sahrendt0@gmail.com
+# Date: '.$date.'
+##################################
+use warnings;
+use strict;
+use Getopt::Long;
+
+
+#####-----Global Variables-----#####
+my $input;
+my ($help,$verb);
+
+GetOptions (\'i|input=s\' => \\$input,
+            \'h|help\'   => \\$help,
+            \'v|verbose\' => \\$verb);
+my $usage = "Usage: '.$input.' -i input\n";
+die $usage if $help;
+
+warn "Done.\n";
+exit(0);
+
+#####-----Subroutines-----#####';
+close(OUT);
+
+print `chmod 744 $input`;
