a617dc60e0692863f49a88d777e21c6f2ac3787b
Steven Ahrendt <sahrendt0@gmail.com>
2014-01-30 11:03:29 -0800

    Updated: allows single fastafile input, rather than whole dir

--- hmm_run.pl
+++ hmm_run.pl
@@ -3,7 +3,7 @@
 # Description: Generates a shell script to run batch searches using either hmmsearch or hmmscan
 # Author: Steven Ahrendt
 # email: sahrendt0@gmail.com
-# Date: 4.11.13
+# Date: 1.30.14
 ####################################
 # Usage: perl hmm_run.pl -p hmmprogram -i hmmfile -d directory_of_proteomes -e inclusion_eval_threshold -t description
 #####################################
@@ -18,66 +18,65 @@ use strict;
 use warnings;
 use Getopt::Long;
 use Cwd;
-
+#####-----Global Variables-----#####
 my $hmmfile = "";
 my $dir = ".";
 my $incE = 0;
 my $abbr = "";
 my $prog = "";
 my $help = 0;
-#GetOptions ('verbose'    => \$verbose, 'all' => \$all);
+my $fastafile;
 GetOptions ('i|input=s'   => \$hmmfile, 
+            'f|fasta=s'   => \$fastafile,
             'd|dir=s'     => \$dir,
             'e|eval=s'    => \$incE,
             't|type=s'    => \$abbr,
             'p|program=s' => \$prog,
             'h|help+'     => \$help);
+my $usage = "Usage: hmm_run.pl -p hmmprogram -i hmmfile (-f fasta_file | -d proteome_dir) -e eval_threshold -t description\nCreates an executable shell script.\n";
+die $usage if ($help);
+die "Can't open $fastafile: $!\n$usage" if (!(-e $fastafile));
+die "Invalid hmmprogram: $prog\n\"hmmsearch\" or \"hmmscan\" only.\n$usage" if (($prog ne "hmmscan") and ($prog ne "hmmsearch"));
+die "Please provide a description.\n$usage" if ($abbr eq "");
+die "Invalid hmm profile: $hmmfile\n$usage" if ($hmmfile eq "");
 
-if($help)
-{
-  print "Usage: hmm_run.pl -p hmmprogram -i hmmfile -d proteome_dir -e eval_threshold -t description\n";
-  exit;
-}
-
+#####-----Main-----#####
 ## Create the shell script
 #print $hmmfile,"\n";
 open(OUT,">","$abbr\_$prog.sh");
-if(($hmmfile ne "") && ($abbr ne "") && (($prog eq "hmmscan") || ($prog eq "hmmsearch")))
+if(#read whole dir)
 {
   opendir(DIR,$dir);
-  my @fasta_files = grep {/\.fasta$/} readdir(DIR);
+  @fasta_files = grep {/\.fasta$/} readdir(DIR);
   closedir(DIR);
-  print OUT "cd ",cwd(),"\n";  
-  foreach my $fasta_file (@fasta_files)
-  {
-    my $outfilename;
-    if($prog eq "hmmscan")
-    {
-      $outfilename = join("-",substr($fasta_file,0,4),"vs",$abbr);
-    } #hmmscan
-    else
-    {
-      $outfilename = join("-",$abbr,"vs",substr($fasta_file,0,4));
-    } #hmmsearch
-    print OUT "$prog ";
-    print OUT "--noali ";
-    if($incE){print OUT "--incE $incE -E $incE ";}
-    print OUT "--tblout ",join("_",$outfilename,"tbl"),".$prog ";
-    print OUT "-o $outfilename\.$prog ";
-    print OUT "$hmmfile ";  
-    print OUT "$dir/$fasta_file";
-
-#    print " > ";
-#    print $abbr;
-#    print "-vs-",substr($fasta_file,0,3),".$prog";
-    print OUT "\n";
-  }
-close(OUT);
-print `chmod 744 $abbr\_$prog.sh`;
 }
 else
 {
-  print "Please provide a vaild hmm profile, a valid hmmprogram (hmmsearch or hmmscan), and a valid search description\n";
-  print " Usage: perl hmm_run.pl -p hmmprogram -i hmmfile -d directory_of_proteomes -e inclusion_eval_threshold -t description\n";
+  push @fasta_files,$fastafile;
 }
+print OUT "cd ",cwd(),"\n";  
+foreach my $fasta_file (@fasta_files)
+{
+  my $outfilename;
+  if($prog eq "hmmscan")
+  {
+    $outfilename = join("-",substr($fasta_file,0,4),"vs",$abbr);
+  } #hmmscan
+  else
+  {
+    $outfilename = join("-",$abbr,"vs",substr($fasta_file,0,4));
+  } #hmmsearch
+  print OUT "$prog ";
+  print OUT "--noali ";
+  if($incE){print OUT "--incE $incE -E $incE ";}
+  print OUT "--tblout ",join("_",$outfilename,"tbl"),".$prog ";
+  print OUT "-o $outfilename\.$prog ";
+  print OUT "$hmmfile ";  
+  print OUT "$dir/$fasta_file";
+  print OUT "\n";
+}
+close(OUT);
+print `chmod 744 $abbr\_$prog.sh`;
 
+warn "Done.\n";
+exit(0);
