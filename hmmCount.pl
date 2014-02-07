a8785cca3d204a2b3063923b7c6e29262ded73e4
Steven Ahrendt <sahrendt0@gmail.com>
2014-02-07 14:42:24 -0800

    General script to get counts from HMM results file

---
+++ hmmCount.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+# Script: hmmCount.pl
+# Description: General script to get counts from HMM results file 
+# Author: Steven Ahrendt
+# email: sahrendt0@gmail.com
+# Date: 01.31.2014
+##################################
+use warnings;
+use strict;
+use Getopt::Long;
+use SeqAnalysis;
+use Data::Dumper;
+#####-----Global Variables-----#####
+my $input;
+my ($help,$verb);
+
+GetOptions ('i|input=s' => \$input,
+            'h|help'   => \$help,
+            'v|verbose' => \$verb);
+my $usage = "Usage: hmmCount.pl -i input\nWrites to file: $input\.counts\n";
+die $usage if $help;
+die "No input.\n$usage" if (!$input);
+
+#####-----Main-----#####
+my %counts = hmmParse($input);
+
+open(OUT,">$input\.counts");
+foreach my $key (sort keys %counts)
+{
+  print OUT "$key\t$counts{$key}\n";
+}
+close(OUT);
+warn "Done.\n";
+exit(0);
+
+#####-----Subroutines-----#####
