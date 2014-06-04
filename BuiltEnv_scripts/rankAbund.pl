75b8695a502fcf9b582092bbef983d16281c521e
sahrendt0 <sahrendt0@gmail.com>
2014-06-04 18:22:13 -0400

    Rank abundance

---
+++ BuiltEnv_scripts/rankAbund.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+# Script: rankAbund.pl
+# Description:  
+# Author: Steven Ahrendt
+# email: sahrendt0@gmail.com
+# Date: 06.04.2014
+##################################
+use warnings;
+use strict;
+use Getopt::Long;
+use Data::Dumper;
+
+#####-----Global Variables-----#####
+my $input;
+my ($help,$verb);
+my %hash;
+GetOptions ('i|input=s' => \$input,
+            'h|help'   => \$help,
+            'v|verbose' => \$verb);
+my $usage = "Usage: rankAbund.pl -i input\n";
+die $usage if $help;
+die "No input.\n$usage" if (!$input);
+
+#####-----Main-----#####
+open(my $fh,"<",$input) or die "Can't open $input: $!\n";
+my @hash_keys;
+while(my $line = <$fh>)
+{
+  chomp $line;
+  if($line =~ /^Taxon/)
+  {
+    @hash_keys = split(/\t/,$line);
+    shift @hash_keys;
+  }
+  else
+  {
+    my ($level,@abund) = split(/\t/,$line);
+    for(my $i=0;$i<scalar(@hash_keys); $i++)
+    {
+      $hash{$hash_keys[$i]}{$level} = $abund[$i];
+    }
+  }
+}
+close($fh);
+
+#print Dumper \%hash;
+foreach my $key (sort keys %hash)
+{
+  my $count = 0;
+  print $key,"\n";
+  foreach my $level (sort {$hash{$key}{$b} <=> $hash{$key}{$a} } keys $hash{$key})
+  {
+    print "\t",$level,"\t",$hash{$key}{$level},"\n" if($count < 5);
+    $count++;
+  }
+}
+warn "Done.\n";
+exit(0);
+
+#####-----Subroutines-----#####
