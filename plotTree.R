29a16822c14b1d1d4e91a0ff0b4814e2f869c31b
Steven Ahrendt <sahrendt0@gmail.com>
2014-11-24 09:44:35 -0800

    Script for plotting trees

--- plotTree.R
+++ plotTree.R
@@ -1,18 +1,33 @@
+#!/usr/bin/env Rscript
 # Description: Uses ape package to color a tree based on a specific color scheme
 #install.packages("ape")
 library(ape)
-myTree <- read.tree("RAxML_bestTree.mafft.trim.rxml2.newick")
+
+## Read in Newick tree
+myTree <- read.tree("newickTree")
+
+## Read in colorconfig files and set up tip label <=> color map
 colorConfig <- read.delim("colorMap.config",sep="\t",header=T)
 colorCode <- as.vector(unlist(lapply(myTree$tip.label,function(x) colorConfig$Color[which(x == colorConfig$Taxa)])))
 label <- as.vector(unlist(lapply(myTree$tip.label,function(x) colorConfig$Taxa[which(x == colorConfig$Taxa)])))
 
-pdf(file="plot.pdf",30,10)
+pdf(file="plot.pdf",60,30)
 plot(myTree,show.tip.label=FALSE)
-tiplabels(col=colorCode,frame="n",text=label,cex=0.7,adj=-0.05)
-add.scale.bar()
+
+## label tips w/ names
+tiplabels(col=colorCode,frame="n",text=label,cex=0.8,adj=-0.05)
+
+## label nodes w/ bootstrap values >= 45
+nodelabels(text=myTree$node.label[which(as.integer(myTree$node.label) >= 45)],node=(which(as.integer(myTree$node.label) >= 45)+myTree$Nnode+2),bg="white",frame="none",adj=c(1.1,1.5))
+
+## Scale bar
+add.scale.bar(x=5,y=3,length=1,pos=1)
 
 ## Making the legend
-orders <- unique(as.vector(unlist(lapply(myTree$tip.label,function(x) colorConfig$Order[which(x == colorConfig$Taxa)]))))
-leg_cols <- unique(as.vector(unlist(lapply(orders,function(x) colorConfig$Color[which(x == colorConfig$Order)]))))
-legend("center",legend=orders,cex=1,text.col=leg_cols)
+groups <- unique(as.vector(unlist(lapply(myTree$tip.label,function(x) colorConfig$Group[which(x == colorConfig$Taxa)]))))
+leg_cols <- unique(as.vector(unlist(lapply(groups,function(x) colorConfig$Color[which(x == colorConfig$Group)]))))
+legend("center",legend=groups,cex=1,text.col=leg_cols)
 dev.off()
+
+## Launch pdf viewer
+system("evince plot.pdf &")
