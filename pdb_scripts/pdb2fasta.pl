1c88ad3f37f56a17523a35153eda934b9013f9df
Steven Ahrendt <sahrendt0@gmail.com>
2015-01-16 10:48:56 -0800

    PDB 2 fasta converter

---
+++ pdb_scripts/pdb2fasta.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+# Script: pdb2fasta.pl
+# Description: Gets fasta sequence from pdb file
+# Author: Steven Ahrendt
+# email: sahrendt0@gmail.com
+# Date: 01.03.2015
+##################################
+use warnings;
+use strict;
+use Getopt::Long;
+use lib '/rhome/sahrendt/Scripts';
+use lib '/rhome/sahrendt/Scripts/pdb_scripts';
+use ParsePDB;
+use PDBAnalysis;
+use Bio::Seq;
+use Bio::SeqIO;
+
+#####-----Global Variables-----#####
+my $input;
+my ($help,$verb);
+
+GetOptions ('i|input=s' => \$input,
+            'h|help'   => \$help,
+            'v|verbose' => \$verb);
+my $usage = "Usage: pdb2fasta.pl -i input\nGets fasta sequence from pdb file\n";
+die $usage if $help;
+die "No input.\n$usage" if (!$input);
+
+#####-----Main-----#####
+my $seq_obj = getSeqFromFile($input);
+my $seqio_obj = Bio::SeqIO->new(-format => "fasta",
+                                -file => ">$input.faa");
+
+$seqio_obj->write_seq($seq_obj);
+
+warn "Done.\n";
+exit(0);
+
+#####-----Subroutines-----#####
