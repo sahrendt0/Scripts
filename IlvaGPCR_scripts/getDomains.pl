70738ccc56ee8bdfb97f0e0d34428cda5cff51bf
Steven Ahrendt <sahrendt0@gmail.com>
2015-04-03 10:48:48 -0700

    Started adding code for consensus

--- IlvaGPCR_scripts/getDomains.pl
+++ IlvaGPCR_scripts/getDomains.pl
@@ -18,6 +18,7 @@ use Bio::SeqIO;
 my $input;
 my ($help,$verb);
 my %TMHMM; # hash for parsing TMHMMfile (short format)
+my %PHOBIUS; # hash for parsing PHOBIUSfile (short format)
 my $seqIn_obj; # fasta file; Bio::SeqIO object
 my $seqOut_obj; # fasta file; Bio::SeqIO object; for helices
 
@@ -30,6 +31,7 @@ die "No input.\n$usage" if (!$input);
 
 #####-----Main-----#####
 %TMHMM = %{tmhmmParse("$input\.tmhmm")};
+%PHOBIUS = %{phobiusParse("$input\.phobius")};
 $seqIn_obj = Bio::SeqIO->new(-file => "$input\.fasta",
                              -format => "fasta");
 $seqOut_obj = Bio::SeqIO->new(-file => ">$input\.out.fasta",
@@ -37,14 +39,31 @@ $seqOut_obj = Bio::SeqIO->new(-file => ">$input\.out.fasta",
 
 while(my $seq_obj = $seqIn_obj->next_seq)
 {
-  
-  my $topo = $TMHMM{$seq_obj->display_id}{"Topology"};
-  $topo =~ s/^[oi]//;
-  my @helices = split(/[oi]/,$topo);
-  print $seq_obj->display_id,"\n" if ($verb);
+  my $seqID = $seq_obj->display_id;
+  if($TMHMM{$seqID}{"PredHel"} != $PHOBIUS{$seqID}{"PredHel"})
+  {
+    next;
+  }
+  else
+  {
+    ## Get consensus of helices
+    my $TMtopo = $TMHMM{$seqID}{"Topology"};
+    my $PHtopo = $PHOBIUS{$seqID}{"Topology"};
+    $TMtopo =~ s/^[oi]//;
+    $PHtopo =~ s/^[oi]//;
+    my @TMhelices = split(/[oi]/,$TMtopo);
+    my @PHhelices = split(/[oi]/,$PHtopo);
+    for(my $i=0;$i<scalar(@TMhelices);$i++)
+    {
+      print "$TMhelices{$i}\n";
+      print "$PHhelices{$i}\n\n";
+    }
+  }
+}
+__END__
   my $newSeq = "";
   foreach my $helix (@helices)
-  {
+  {  
     my ($start,$stop) = split(/-/,$helix);
     $start-=1;
     my $len = $stop-$start;
